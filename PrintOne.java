76981585cf9ffbfffe42ab631d04326f9ba7d9a9
Dev Shrivastava <devshrivastava621@gmail.com>
2026-03-12 12:55:43 +0530

    initial commit

---
+++ PrintOne.java
@@ -0,0 +1,45 @@
+import java.util.*;
+public class PrintOne
+{
+    static boolean printS(int ind, ArrayList<Integer> ds, int s, int sum, int arr[], int n){
+        if(ind == n)
+        {
+            if(s== sum)
+            {
+                for(int it: ds)
+                {
+                    System.out.print(it + " ");
+                } 
+                System.out.println();
+                return true;
+            }
+                return false;
+        }
+        ds.add(arr[ind]);
+        s += arr[ind];
+
+        if(printS(ind + 1,ds,s,sum,arr, n))
+        {
+            return true;
+        }
+        // BACKTRACK
+        s -= arr[ind];
+        ds.remove(ds.size() - 1);
+
+        if(printS(ind + 1,ds , s, sum, arr ,n))
+        {
+            return true;
+
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        int arr[] = {1,2,1};
+        int n = 3;
+        int sum = 2;
+
+        ArrayList<Integer> ds = new ArrayList<>();
+
+        printS(0,ds,0,sum,arr,n);
+    }
+}
